9a20c568b183a9de0c6df34873052cc221923eb9
Fabio Santos <fabiosan@microsoft.com>
2023-08-02 15:38:05 -0700

    upgrade

--- simpleProviderManaged/EnvironmentHelper.cs
+++ simpleProviderManaged/EnvironmentHelper.cs
@@ -13,10 +13,11 @@ namespace SimpleProviderManaged
         {
             // Using registry instead of OSVersion due to https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getversionexa?redirectedfrom=MSDN.
             // This code can be replaced with Environment.OSVersion on .NET Core 5 and higher.
-            int build = Convert.ToInt32(Microsoft.Win32.Registry.GetValue(
+            int build = Environment.OSVersion.Version.Build; 
+            /*Convert.ToInt32(Microsoft.Win32.Registry.GetValue(
                 @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion",
                 "CurrentBuild",
-                0));
+                0));*/
 
             if (build >= 19041)
             {
